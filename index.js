f3ffebd833d7cbe2047d1d60dd36fc0a6ff3c0a6
leplayer <lennoncodesso500@gmail.com>
2020-02-13 21:23:06 -0300

    No comments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-  if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP 
-      res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
-  else //Se a requisição já é HTTPS 
-      next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+app.use((req, res, next) => {
+  if (req.headers["x-forwarded-proto"] == "http")
+      res.redirect(`https://${req.headers.host}${req.url}`);
+  else 
+      next(); 
 });
 
 app.get('/', function (req, res) {
